380dade121190be703e73ef8e812fc9a59ae0d85
Skryam <skryams@gmail.com>
2024-12-19 16:57:39 +0500

    u

---
+++ server/routes/changeLang.js
@@ -0,0 +1,11 @@
+
+import i18next from 'i18next';
+
+export default (app) => {
+  app
+    .post('/change-language', { name: 'changeLang' }, (req, reply) => {
+      const lng = i18next.language === 'en' ? 'ru' : 'en';
+      i18next.changeLanguage(lng);
+      reply.redirect(app.reverse('root'));
+  })
+}
--- server/routes/index.js
+++ server/routes/index.js
@@ -3,11 +3,13 @@
 import welcome from './welcome.js';
 import users from './users.js';
 import session from './session.js';
+import changeLang from './changeLang.js';
 
 const controllers = [
   welcome,
   users,
   session,
+  changeLang,
 ];
 
 export default (app) => controllers.forEach((f) => f(app));
