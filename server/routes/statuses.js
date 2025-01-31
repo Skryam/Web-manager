b136f1944468e59f8405b437251eb255dd1c1223
Skryam <skryams@gmail.com>
2025-01-31 17:35:25 +0500

    u

--- server/routes/statuses.js
+++ server/routes/statuses.js
@@ -40,16 +40,15 @@ export default (app) => {
     })
     .patch('/statuses/:id', async (req, reply) => {
       app.authenticate(req, reply);
-      const newData = req.body.data;
+      const status = await app.objection.models.status.query().findById(req.params.id);
 
       try {
-        const status = await app.objection.models.user.query().findById(req.params.id);
-        await status.$query().patch({ name: newData.name });
+        await status.$query().patch({ name: req.body.data.name });
         req.flash('info', i18next.t('flash.statuses.patch.success'));
         reply.redirect(app.reverse('statuses'));
       } catch (err) {
         req.flash('error', i18next.t('flash.statuses.patch.error'));
-        reply.render('statuses/edit', { status: newData, errors: err });
+        reply.render('statuses/edit', { data: status, errors: err });
       }
 
       return reply;
