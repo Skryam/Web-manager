9d7ae2d60dbc7245c15ad49a12c5b28debeb544b
Skryam <skryams@gmail.com>
2025-02-10 17:51:16 +0500

    u

--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -7,6 +7,7 @@ export default (app) => {
     .get('/tasks', { name: 'tasks' }, async (req, reply) => {
       app.authenticate(req, reply);
       const tasks = await app.objection.models.task.query().withGraphFetched('[status, creator]');
+      console.log(tasks);
       reply.render('tasks/index', { tasks });
       return reply;
     })
@@ -25,18 +26,21 @@ export default (app) => {
         ...req.body.data,
         creatorId: req.user.id.toString(),
       };
-      console.log('!!!!!!!!!!!!!!!!!', taskData)
 
       try {
         const validTask = await app.objection.models.task.fromJson(taskData);
-        console.log('33333333333333333333', validTask)
         await app.objection.models.task.query().insert(validTask);
         req.flash('info', 'norm');
         reply.redirect(app.reverse('tasks'));
       } catch (errors) {
-        console.log('################################', errors);
+        console.log('####################', errors)
+        const task = new app.objection.models.task();
+        const statuses = await app.objection.models.status.query();
+        const users = await app.objection.models.user.query();
         req.flash('error', 'upal');
-        reply.render('tasks/new', { task: req.body.data, errors });
+        reply.render('tasks/new', {
+          task, statuses, users, errors: errors.data,
+        });
       }
 
       return reply;
