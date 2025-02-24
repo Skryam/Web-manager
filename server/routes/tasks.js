b179b227cc80557cf42c8da54a7df43a933e3325
Skryam <skryams@gmail.com>
2025-02-24 17:57:24 +0500

    u

--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -35,6 +35,10 @@ export default (app) => {
       try {
         const validTask = await app.objection.models.task.fromJson(taskData);
         await app.objection.models.task.query().insert(validTask);
+        console.log('iiiiiiiiid', validTask.$id());
+        await app.objection.models.tasksLabels.query()
+          .insert({ taskId: validTask.$id(), labelId: data.labels });
+
         req.flash('info', i18next.t('flash.tasks.create.success'));
         reply.redirect(app.reverse('tasks'));
       } catch (errors) {
@@ -53,7 +57,7 @@ export default (app) => {
     .get('/tasks/:id', async (req, reply) => {
       app.authenticate(req, reply);
       const task = await app.objection.models.task.query().findById(req.params.id).withGraphFetched('[status, creator, executor, labels]');
-      console.log('taaaaaaaaaaask', task)
+      console.log('taaaaaaaaaaask', task);
       reply.render('tasks/view', { task });
       return reply;
     })
@@ -71,22 +75,27 @@ export default (app) => {
     .patch('/tasks/:id', async (req, reply) => {
       app.authenticate(req, reply);
       const { data } = req.body;
-      console.log('paaaaaaaaaaatch', data)
+      console.log('paaaaaaaaaaatch', data);
       data.statusId = parseInt(data.statusId, 10) || 0;
       data.executorId = parseInt(data.executorId, 10) || null;
 
-      const task = await app.objection.models.task.query().findById(req.params.id).withGraphFetched('[status, creator, executor]');
+      const task = await app.objection.models.task.query().findById(req.params.id).withGraphFetched('[status, creator, executor, labels]');
 
       try {
         await task.$query().patch(data);
+        if (data.labels.length > 0) {
+          await task.$relatedQuery('labels').relate(data.labels);
+        }
         req.flash('info', i18next.t('flash.tasks.patch.success'));
         reply.redirect(app.reverse('tasks'));
       } catch (errors) {
+        console.log(errors)
         const statuses = await app.objection.models.status.query();
         const users = await app.objection.models.user.query();
+        const labels = await app.objection.models.label.query();
         req.flash('error', i18next.t('flash.tasks.patch.error'));
         reply.render('tasks/edit', {
-          task, statuses, users, errors: errors.data,
+          task, statuses, users, labels, errors: errors.data,
         });
       }
 
