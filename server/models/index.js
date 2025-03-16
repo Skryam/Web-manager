492a71f76f598ab12f82510f4fe0333f986cdcaf
Zahar Solovcov <skryams@gmail.com>
2025-03-17 00:00:03 +0500

    u

--- server/models/index.js
+++ server/models/index.js
@@ -4,12 +4,12 @@ import User from './User.cjs';
 import Status from './Status.cjs';
 import Task from './Task.cjs';
 import Label from './Label.cjs';
-import tasksLabels from './TasksLabels.cjs';
+import TasksLabels from './TasksLabels.cjs';
 
 export default [
   User,
   Status,
   Task,
   Label,
-  tasksLabels,
+  TasksLabels,
 ];
